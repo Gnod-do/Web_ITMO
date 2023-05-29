c9dcba68607189973f91ad82ce736607694dca5d
TranDucDuy_ЧанДыкЗюи <tranduybthv@gmail.com>
2023-05-30 00:35:54 +0300

    test 2 demo

---
+++ components/Test2/MyCircle.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: 'flex';
+    justify-content: 'center';
+    align-items: 'center';
+    height: '100vh';
+    svg {
+        height: 1000px;
+    }
+    background-color: #ccc;
+`;
--- pages/test2.tsx
+++ pages/test2.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+// import MovingObjectComponent, {
+//     Circle,
+//     MovingObject,
+// } from '../components/Test2';
+import Canvas from '../components/Test2';
 
 const test2 = () => {
-    return <div>test2</div>;
+    // const circle: Circle = {
+    //     center: { x: 0, y: 0 },
+    //     radius: 10,
+    // };
+
+    // const object: MovingObject = {
+    //     circle,
+    //     velocity: 0,
+    //     acceleration: 2,
+    //     time: 0,
+    //     destination: { x: 20, y: 0 },
+    // };
+    return (
+        <svg viewBox="-50 -50 100 100" width="500" height="500">
+            <Canvas />
+        </svg>
+    );
 };
 
 export default test2;
